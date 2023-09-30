12c3978f52b2d504a52255a6a44bb826b552ea8b
PaulNuffer <78831963+PaulNuffer@users.noreply.github.com>
2023-09-30 15:27:34 -0600

    spawning is now random on edges of arena

--- world.gd
+++ world.gd
@@ -4,10 +4,22 @@ extends Node2D
 @export var worker_scene: PackedScene
 
 
-func _on_timer_timeout():
+
+func _on_spawn_timer_timeout():
 	var guard = guard_scene.instantiate()
 	var worker = worker_scene.instantiate()
-	guard.position = Vector2(randi() % 1500, -20)
-	worker.position = Vector2(randi() % 1500, -20)
+	
+	
+	var guard_spawn_location = get_node("SpawnPath/SpawnLocation")
+	guard_spawn_location.progress_ratio = randf()
+	print(guard_spawn_location.progress_ratio)
+	print(guard_spawn_location.position)
+	var worker_spawn_location = get_node("SpawnPath/SpawnLocation")
+	worker_spawn_location.progress_ratio = randf()
+	print(worker_spawn_location.progress_ratio)
+	print(worker_spawn_location.position)
+	
+	guard.position = guard_spawn_location.position
+	worker.position = worker_spawn_location.position
 	add_child(guard)
 	add_child(worker)
