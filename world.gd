45a574a9c9710b93b37d55225c1b849de5a7f9bd
PaulNuffer <78831963+PaulNuffer@users.noreply.github.com>
2023-09-30 18:45:01 -0600

    Wave system start real

--- world.gd
+++ world.gd
@@ -7,24 +7,28 @@ var wave_number = 1
 
 var wave_array = [[10, .5], [15, .7], [20, 1]]
 
-var alive_guards = 0
+var spawned = 0
 
 
 func _on_spawn_timer_timeout():
-	var guard = guard_scene.instantiate()
-	var worker = worker_scene.instantiate()
 	
-	
-	var guard_spawn_location = get_node("SpawnPath/SpawnLocation")
-	guard_spawn_location.progress_ratio = randf()
-	print(guard_spawn_location.progress_ratio)
-	print(guard_spawn_location.position)
-	var worker_spawn_location = get_node("SpawnPath/SpawnLocation")
-	worker_spawn_location.progress_ratio = randf()
-	print(worker_spawn_location.progress_ratio)
-	print(worker_spawn_location.position)
-	
-	guard.position = guard_spawn_location.position
-	worker.position = worker_spawn_location.position
-	add_child(guard)
-	add_child(worker)
+	if spawned < wave_array[wave_number][0]:
+		var typerand = randf()
+		if typerand < wave_array[wave_number][1]:
+			var guard = guard_scene.instantiate()
+			var guard_spawn_location = get_node("SpawnPath/SpawnLocation")
+			guard_spawn_location.progress_ratio = randf()
+			guard.position = guard_spawn_location.position
+			add_child(guard)
+		else:
+			var worker = worker_scene.instantiate()
+			var worker_spawn_location = get_node("SpawnPath/SpawnLocation")
+			worker_spawn_location.progress_ratio = randf()
+			worker.position = worker_spawn_location.position
+			add_child(worker)
+			
+		
+		
+		
+		
+		
